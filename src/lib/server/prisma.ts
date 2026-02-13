9151e6ab09c641070ec1694a5a2558f1edc3ee56
IEVM <Lukas.Wipf@gmx.ch>
2026-02-13 13:23:58 +0100

    Fix prisma again

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -11,7 +11,11 @@ export function getPrisma() {
 	if (globalForPrisma.prisma) return globalForPrisma.prisma;
 
 	const connectionString =
-		env.POSTGRES_URL_NON_POOLING ?? env.POSTGRES_PRISMA_URL ?? env.POSTGRES_URL ?? '';
+		env.POSTGRES_URL_OVERRIDE ??
+		env.POSTGRES_URL_NON_POOLING ??
+		env.POSTGRES_PRISMA_URL ??
+		env.POSTGRES_URL ??
+		'';
 	if (!connectionString) {
 		throw new Error('POSTGRES_PRISMA_URL is not configured');
 	}
